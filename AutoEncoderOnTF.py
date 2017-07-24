e5d35bf61f021681f6e39f72af14cd8ab6832c25
tianyunkeml <tianyunke666@gmail.com>
2017-07-24 15:50:06 -0400

    Finished the understanding and implementation of whole example with output

--- AutoEncoderOnTF.py
+++ AutoEncoderOnTF.py
@@ -1,10 +1,12 @@
+# Example referenced from the website: http://blog.csdn.net/marsjhao/article/details/68950697
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
 # import MNIST data here
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_dta.read_data_sets('MNIST_data/', one_hot = False)
+mnist = input_data.read_data_sets('MNIST_data/', one_hot = False)
 
 # set tf learning parameters
 learning_rate = 0.01
@@ -33,5 +35,63 @@ biases = {
 	'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
 	'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
 	'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
-	'decoder_b2': tf.Variable(tf.random_normal[n_input])
+	'decoder_b2': tf.Variable(tf.random_normal([n_input]))
 }
+
+# build 2-layers encoder
+def encoder(x):
+	layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), 
+		biases['encoder_b1']))
+	layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+		biases['encoder_b2']))
+	return layer_2
+
+# build 2-layers decoder
+def decoder(x):
+	layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+		biases['decoder_b1']))
+	layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+		biases['decoder_b2']))
+	return layer_2
+
+# build model
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+# prediction variable
+y_pred = decoder_op
+y_true = X
+
+# lost function and optimiser
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+optimiser = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+# start session
+with tf.Session() as sess:
+	# tf.initialize_all_variables() no long valid from  
+    # 2017-03-02 if using tensorflow >= 0.12  
+    if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    	init = tf.initialize_all_variables()
+    else:
+    	init = tf.global_variables_initializer()
+    sess.run(init)	# FIRST: INIT
+
+    # compute batch number
+    total_batch = int(mnist.train.num_examples / batch_size)
+    for epoch in range(training_epochs):
+    	for i in range(total_batch):
+    		batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+    		# SECOND: RUN SESSION WITH COST, OPTIMISER AND FEED_DICT
+    		_, c = sess.run([optimiser, cost], feed_dict = {X: batch_xs})
+    	if epoch % display_step == 0:
+    		print('Epoch:', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(c))
+    print('Optimization Finished!')
+
+    # use the trained encoder to encode and then decode to see how similar
+    encode_decode = sess.run(
+    	y_pred, feed_dict = {X: mnist.test.images[:examples_to_show]})
+    f, a = plt.subplots(2, 10, figsize = (10, 2))
+    for i in range(examples_to_show):
+    	a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+    	a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
+    plt.show()
